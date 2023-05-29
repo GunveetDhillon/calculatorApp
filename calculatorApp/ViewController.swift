3bbd937eafc1db8d7b5d036ea5a65dc39b65bb15
GunveetDhillon <dhillon.gunu@gmail.com>
2023-05-29 16:01:19 -0400

    Initial Commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by Gunveet Singh Dhillon on 2023-05-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ calculatorAppUITests/calculatorAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  calculatorAppUITestsLaunchTests.swift
+//  calculatorAppUITests
+//
+//  Created by Gunveet Singh Dhillon on 2023-05-29.
+//
+
+import XCTest
+
+final class calculatorAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
